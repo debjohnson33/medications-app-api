d488a7cd1f978b501cfa655e8ed9fc76b4ac3102
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-13 14:05:31 -0600

    Added name to medication attributes

--- app/serializers/medication_serializer.rb
+++ app/serializers/medication_serializer.rb
@@ -1,4 +1,4 @@
 class MedicationSerializer < ActiveModel::Serializer
-  attributes :id, :generic_name, :uses, :side_effects, :precautions
+  attributes :id, :name, :generic_name, :uses, :side_effects, :precautions
   has_many :reviews
 end
