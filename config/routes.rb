e840de78d437a3f41d5bd0efeebc4be36255720b
debjohnson33 <debjohnson_11@yahoo.com>
2018-01-22 13:00:37 -0600

    Changed routes for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   namespace :api do
-  	resources :medications
   	resources :reviews
+  	resources :medications do
+  		resources :reviews, :onnly =>[:index, :show]
+  	end
   end
 end
