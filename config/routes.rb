b517dd74796ae440d57b8a153a2996c7553d5bc5
debjohnson33 <debjohnson_11@yahoo.com>
2018-02-15 17:47:31 -0600

    Fixed spelling mistake

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
   	resources :reviews
   	resources :medications do
-  		resources :reviews, :onnly =>[:index, :show]
+  		resources :reviews, :only =>[:index, :show]
   	end
   end
 end
